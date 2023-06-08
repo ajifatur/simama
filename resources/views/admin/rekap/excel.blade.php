a8b40ec23e3cd81d1aa7d21f8e839ab731d5ebdc
Fathurrahman Prasetyo Aji <ajifatur2@gmail.com>
2023-06-08 07:32:54 +0700

    Update excel.blade.php

--- resources/views/admin/rekap/excel.blade.php
+++ resources/views/admin/rekap/excel.blade.php
@@ -1,3 +1,4 @@
+@if($data['all'] != true)
 <table border="1" style="width: 100%">
     <thead>
         <tr>
@@ -44,3 +45,59 @@
         @endforeach
     </tbody>
 </table>
+@else
+<table border="1" style="width: 100%">
+    <thead>
+        <tr>
+            <th width="5">No</th>
+            <th width="50">Nama</th>
+            <th width="15">Status</th>
+            <th width="15">Unit</th>
+            <th colspan="2">Tanda Tangan</th>
+        </tr>
+    </thead>
+    <tbody>
+        @php $i = 1; @endphp
+		@foreach($data['unit'] as $u)
+			<?php
+			$purnakarya = \App\Models\Purnakarya::where('unit_id','=',$u->id)->where('status','1')->orderBy('nama','asc')->get();
+			$warakawuri = \App\Models\Warakawuri::whereHas('purnakarya', function (\Illuminate\Database\Eloquent\Builder $query) use ($u) {
+				return $query->where('unit_id','=',$u->id);
+			})->where('status','1')->orderBy('nama','asc')->get();
+			?>
+			@foreach($purnakarya as $p)
+			<tr>
+				<td>{{ $i }}</td>
+				<td>{{ $p->nama }}</td>
+				<td>Purnakarya</td>
+				<td>{{ $p->unit->nama }}</td>
+				@if($i % 2 == 1)
+					<td width="20" align="left">{{ $i }}.</td>
+					<td width="20" align="left"></td>
+				@else
+					<td width="20" align="left"></td>
+					<td width="20" align="left">{{ $i }}.</td>
+				@endif
+			</tr>
+			@php $i++ @endphp
+			@endforeach
+			@foreach($warakawuri as $w)
+			<tr>
+				<td>{{ $i }}</td>
+				<td>{{ $w->nama }}</td>
+				<td>Warakawuri</td>
+				<td>{{ $w->purnakarya->unit->nama }}</td>
+				@if($i % 2 == 1)
+					<td width="20" align="left">{{ $i }}.</td>
+					<td width="20" align="left"></td>
+				@else
+					<td width="20" align="left"></td>
+					<td width="20" align="left">{{ $i }}.</td>
+				@endif
+			</tr>
+			@php $i++ @endphp
+			@endforeach
+		@endforeach
+    </tbody>
+</table>
+@endif
